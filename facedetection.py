801b15b68404f046fc6523ec4cca8b095ca79284
Rajendraprasath <59873620+SSRaj001@users.noreply.github.com>
2020-11-08 14:10:22 +0530

    Initial Commit

---
+++ facedetection.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+while(True):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (20,20)
+    )
+    eyes = eyeCascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (5,5)
+        )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w] 
+
+    for (x,y,w,h) in eyes:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w] 
+
+    cv2.imshow('video',img)
+
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
